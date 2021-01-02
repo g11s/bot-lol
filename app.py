0377eb2170887c7fae977362c1f0b9c31f824c67
pourlapaix <perfildeveloper@gmail.com>
2021-01-01 21:47:43 -0300

    refactor: criado classe core com funcoes cruciais e interna

--- app.py
+++ app.py
@@ -1,78 +1,28 @@
-import time
-
 import keyboard
+from core import Core
 
-from screen_manager import ScreenManager
-from client import Client
 
-from config import pathImages
-
-def run():
-    banChampionText = input("Insira o nome do campeão que deseja banir: ")
-    selectChampionText = input(
-        "Insira o nome do campeão que deseja selecionar: ")
-
-    print("\nPronto, agora pode iniciar a partida!\n")
-
-    print("Se precisar trocar o campeão que deseja banir, pressione CTRL + X")
-    print("Se precisar trocar o campeão que deseja selecionar, pressione CTRL + S")
-    print("Se precisar sair, pressione ESQ\n")
+def start():
+    Core.setup()
 
     while True:
         if keyboard.is_pressed("CTRL") and keyboard.is_pressed("X"):
-            banChampionText = input(
-                "Insira o nome do campeão que deseja banir: ")
-            print("\nEntendido capitão!")
-            print(f"Agora baniremos {banChampionText}")
+            Core.changeBan()
 
         if keyboard.is_pressed("CTRL") and keyboard.is_pressed("S"):
-            selectChampionText = input(
-                "Insira o nome do campeão que deseja selecionar: ")
-            print("\nEntendido capitão!")
-            print(f"Agora selecionaremos {selectChampionText}")
+            Core.changeSelect()
 
         if keyboard.is_pressed("ESC"):
             print("\nSaindo do programa BotLol")
             break
 
-        coordinatesAcceptMatch = ScreenManager.search_image_on_screen(
-            image_to_search=pathImages + "AcceptMatch.png")
+        Core.checkNeedAcceptMatch()
 
-        if coordinatesAcceptMatch:
-            Client.acceptMatch(coordinatesAcceptMatch)
+        Core.checkNeedSelectChampion()
 
-        coordinatesMessageDeclareChampion = ScreenManager.search_image_on_screen(
-            image_to_search=pathImages + "MessageDeclareChampion.png")
+        Core.checkNeedBanChampion()
 
-        coordinatesMessageBanChampion = ScreenManager.search_image_on_screen(
-            image_to_search=pathImages + "MessageBanChampion.png")
-
-        coordinatesMessageSelectChampion = ScreenManager.search_image_on_screen(
-            image_to_search=pathImages + "MessageChooseChampion.png")
-
-        if coordinatesMessageDeclareChampion:
-            selectOrBanChampion(selectChampionText, "Declarei")
-
-        if coordinatesMessageBanChampion:
-            selectOrBanChampion(banChampionText, "Bani")
-
-        if coordinatesMessageSelectChampion:
-            selectOrBanChampion(selectChampionText, "Selecionei")
+        Core.checkNeedChooseChampion()
 
 
-def selectOrBanChampion(champion: str, message: str):
-    Client.findInputSearch()
-
-    Client.writeInSearch(champion)
-
-    Client.clickInChampion(champion)
-
-    if message != "Bani":
-        Client.selectChampion()
-    else:
-        Client.banChampion()
-
-    Client.checkIfFinish(message)
-
-
-run()
+start()
---
+++ core.py
@@ -0,0 +1,90 @@
+from screen_manager import ScreenManager
+from config import pathImages
+from client import Client
+
+banChampionText = ''
+selectChampionText = ''
+
+
+class Core:
+
+    @staticmethod
+    def setup():
+        global banChampionText, selectChampionText
+
+        banChampionText = input("Insira o nome do campeão que deseja banir: ")
+        selectChampionText = input(
+            "Insira o nome do campeão que deseja selecionar: ")
+
+        print("\nPronto, agora pode iniciar a partida!\n")
+
+        print("Se precisar trocar o campeão que deseja banir, pressione CTRL + X")
+        print("Se precisar trocar o campeão que deseja selecionar, pressione CTRL + S")
+        print("Se precisar sair, pressione ESQ\n")
+
+    @staticmethod
+    def changeBan():
+        global banChampionText
+
+        banChampionText = input(
+            "Insira o nome do campeão que deseja banir: ")
+
+        print("\nEntendido capitão!")
+        print(f"Agora baniremos {banChampionText}")
+
+    @staticmethod
+    def changeSelect():
+        global selectChampionText
+
+        selectChampionText = input(
+            "Insira o nome do campeão que deseja selecionar: ")
+
+        print("\nEntendido capitão!")
+        print(f"Agora selecionaremos {selectChampionText}")
+
+    @staticmethod
+    def checkNeedAcceptMatch():
+        coordinatesAcceptMatch = ScreenManager.search_image_on_screen(
+            image_to_search=pathImages + "AcceptMatch.png")
+
+        if coordinatesAcceptMatch:
+            Client.acceptMatch(coordinatesAcceptMatch)
+
+    @staticmethod
+    def checkNeedSelectChampion():
+        coordinatesMessageDeclareChampion = ScreenManager.search_image_on_screen(
+            image_to_search=pathImages + "MessageDeclareChampion.png")
+
+        if coordinatesMessageDeclareChampion:
+            selectOrBanChampion(selectChampionText, "Declarei")
+
+    @staticmethod
+    def checkNeedBanChampion():
+        coordinatesMessageBanChampion = ScreenManager.search_image_on_screen(
+            image_to_search=pathImages + "MessageBanChampion.png")
+
+        if coordinatesMessageBanChampion:
+            selectOrBanChampion(banChampionText, "Bani")
+
+    @staticmethod
+    def checkNeedChooseChampion():
+        coordinatesMessageSelectChampion = ScreenManager.search_image_on_screen(
+            image_to_search=pathImages + "MessageChooseChampion.png")
+
+        if coordinatesMessageSelectChampion:
+            selectOrBanChampion(selectChampionText, "Selecionei")
+
+
+def selectOrBanChampion(champion: str, message: str):
+    Client.findInputSearch()
+
+    Client.writeInSearch(champion)
+
+    Client.clickInChampion(champion)
+
+    if message == "Bani":
+        Client.banChampion()
+    else:
+        Client.selectChampion()
+
+    Client.checkIfFinish(message)
